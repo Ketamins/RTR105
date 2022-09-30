2973806c328d1ceffda28e2174bd7f55dc907cd6
Ketamins <matiss2908@gmail.com>
2022-09-30 11:32:58 +0300

    Class_06

---
+++ Class_06/first.c
@@ -0,0 +1,18 @@
+//komentāra viena rinda // - apzīmējums kā sākt rakstīt komentāru
+//kometāra cita rinda
+
+
+/*
+komentāra paragrāfs
+izmanto, piemerām, lai komentetu koda fragmentus
+*/
+
+int main()
+ {
+ //if()
+ //
+ 
+ 
+
+ return 100;
+ }
